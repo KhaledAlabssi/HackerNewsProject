d4c81580b36ec3c09b7e703f8f9ea2e7823ca696
KhaledAlabssi <khaled.absi5@gmail.com>
2021-06-22 07:39:20 +0200

    add list, item, and search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [result, setResult] = useState([
+    {id:1, body: "I'm result 1", author: 'Sam'},
+    {id:2, body: "I'm result 2", author: 'Lee'},
+    {id:3, body: "I'm result 3", author: 'Adam'},
+    
+  ])
   return (
     <div className="App">
+      
       
     </div>
   );
