3efbee25a8d2487d739d2ddd59bb06d04f801e94
KhaledAlabssi <khaled.absi5@gmail.com>
2021-06-23 06:10:22 +0200

    fetching data by useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   const [result, setResult] = useState([])
-  
 
-  fetch('http://hn.algolia.com/api/v1/search?query=')
+  useEffect(() => {
+    fetch('http://hn.algolia.com/api/v1/search?query=')
   .then(response => response.json())
   .then(data => setResult(data.hits));
+
+  }, [])
+  
+
+  
 
   
   return (
