87031e055bdc6bb393e25e332a083a4755e72621
mihail <mihail.soltan@protonmail.ch>
2021-06-24 00:30:43 +0200

    add searchbar

--- src/component/SearchBar.js
+++ src/component/SearchBar.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import {useState} from "react" 
 
 export default function SearchBar() {
-    return (
-        <div>
-            <input type='text' placeholder='Type here...' />
-            
+    const[search, setSearch] = useState('')
+    const searchArticles = (e) =>{
+        setSearch(e.target.value)
+    }
+    return(
+        <div className="searchbar">
+        <input type="text" placeholder="Search" onChange={searchArticles}></input>
+        <button role="submit">Go</button>
         </div>
     )
 }
